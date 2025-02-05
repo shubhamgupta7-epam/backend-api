c7a8b2e251fbcfc904acd46a35060731bc13b3f3
Shubham Gupta7 <shubham_gupta7@epam.com>
2025-02-05 17:36:53 +0530

    4th commit

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
 })
 
+app.get('/api',(req,res) => {
+    res.status(200).json({ message: 'Hello Api!' });
+})
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
