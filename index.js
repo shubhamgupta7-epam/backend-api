c2d7e90ba9d2d9ab0d43cc32c83019bed6ebbbc1
Shubham Gupta7 <shubham_gupta7@epam.com>
2025-02-05 16:44:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+app.use(express.json());
+
+const port = 8080;
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello World!' });
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
