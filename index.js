caa4f58b34f777a7fac6bbf0172774344156277a
Shubham Gupta7 <shubham_gupta7@epam.com>
2025-02-05 18:01:59 +0530

    9th commit

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ app.get('/', (req, res) => {
     res.status(200).json({ message: 'Hello World!' });
 })
 
+app.get('/api', async (req, res) => {
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+        res.status(200).json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
